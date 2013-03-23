6d73df886b3ad775318733f1617609517fb9e0d7
Christopher Roy Bratusek <nano@tuxfamily.org>
2013-03-23 09:53:34 +0100

    automatisms in promptbuilder.py

--- ui/promptbuilder.py
+++ ui/promptbuilder.py
@@ -66,6 +66,8 @@ class PromptBuilder(object):
 			config["Custom"]["prompt"] = widget.get_text(start, end, False)
 
 		self.custom_prompt_buffer.connect("changed", set_custom_prompt)
+		
+		###
 
 		self.place_p_c = gtkbuilder.get_object("place_p_c")
 
@@ -85,226 +87,27 @@ class PromptBuilder(object):
 
 		self.place_ps1.connect("clicked", do_place_ps1)
 
-		def prompt_add(text):
+		def prompt_add(widget, text):
 			if self.active_buffer == "P_C":
 				self.prompt_command_buffer.insert_at_cursor(text)
 			elif self.active_buffer == "PS1":
 				self.custom_prompt_buffer.insert_at_cursor(text)
+
+		def prompt_add_combo(widget, dict):
+			if widget.get_active() != 0:
+				text = dict[widget.get_active()]
+				prompt_add(widget, text)
 
 		def prompt_set(text):
 			if self.active_buffer == "P_C":
 				self.prompt_command_buffer.set_text(text)
 			elif self.active_buffer == "PS1":
 				self.custom_prompt_buffer.set_text(text)
-
-		self.show_toolbox = gtkbuilder.get_object("show_toolbox")
-
-		def do_show_toolbox(widget, data=None):
-			toolbox = gtkbuilder.get_object("Toolbox")
-			toolbox.show_all()
-			toolbox.connect("delete-event", lambda w, e: w.hide() or True)
-
-		self.show_toolbox.connect("clicked", do_show_toolbox)
-
-		self.username = gtkbuilder.get_object("username")
-
-		def set_username(widget, data=None):
-			prompt_add("\\u")
-
-		self.username.connect("clicked", set_username)
-
-		self.hostname = gtkbuilder.get_object("hostname")
-
-		def set_hostname(widget, data=None):
-			prompt_add("\\h")
-
-		self.hostname.connect("clicked", set_hostname)
-
-		self.fhostname = gtkbuilder.get_object("fhostname")
-
-		def set_fhostname(widget, data=None):
-			prompt_add("\\H")
-
-		self.fhostname.connect("clicked", set_fhostname)
-
-		self.time = gtkbuilder.get_object("time")
-
-		def set_time(widget, data=None):
-			prompt_add("\\t")
-
-		self.time.connect("clicked", set_time)
-
-		self.date = gtkbuilder.get_object("date")
-
-		def set_date(widget, data=None):
-			prompt_add("\\d")
-
-		self.date.connect("clicked", set_date)
-
-		self.sign = gtkbuilder.get_object("sign")
-
-		def set_sign(widget, data=None):
-			prompt_add("\\$")
-
-		self.sign.connect("clicked", set_sign)
-
-		self.fworkdir = gtkbuilder.get_object("fworkdir")
-
-		def set_fworkdir(widget, data=None):
-			prompt_add("\\w")
-
-		self.fworkdir.connect("clicked", set_fworkdir)
-
-		self.workdir = gtkbuilder.get_object("workdir")
-
-		def set_workdir(widget, data=None):
-			prompt_add("\\W")
-
-		self.workdir.connect("clicked", set_workdir)
-
-		self.euid = gtkbuilder.get_object("euid")
-
-		def set_euid(widget, data=None):
-			prompt_add("\\$EUID")
-
-		self.euid.connect("clicked", set_euid)
-
-		self.jobs = gtkbuilder.get_object("jobs")
-
-		def set_jobs(widget, data=None):
-			prompt_add("\\j")
-
-		self.jobs.connect("clicked", set_jobs)
-
-		self.bang = gtkbuilder.get_object("bang")
-
-		def set_bang(widget, data=None):
-			prompt_add("\\!")
-
-		self.bang.connect("clicked", set_bang)
-
-		self.number = gtkbuilder.get_object("number")
-
-		def set_number(widget, data=None):
-			prompt_add("\\#")
-
-		self.number.connect("clicked", set_number)
-
-		self.pid = gtkbuilder.get_object("pid")
-
-		def set_pid(widget, data=None):
-			prompt_add("$BASHPID")
-
-		self.pid.connect("clicked", set_pid)
-
-		self.shlvl = gtkbuilder.get_object("shlvl")
-
-		def set_shlvl(widget, data=None):
-			prompt_add("$SHLVL")
-
-		self.shlvl.connect("clicked", set_shlvl)
-
-		self.truncpwd = gtkbuilder.get_object("truncpwd")
-
-		def set_truncpwd(widget, data=None):
-			prompt_add("\\$(trunc_pwd)")
-
-		self.truncpwd.connect("clicked", set_truncpwd)
-
-		self.showsize = gtkbuilder.get_object("showsize")
-
-		def set_showsize(widget, data=None):
-			prompt_add("\\$(show_size)")
-
-		self.showsize.connect("clicked", set_showsize)
-
-		self.countfiles = gtkbuilder.get_object("countfiles")
-		self.countfiles.set_active(0)
-
-		def set_countfiles(widget, data=None):
-			selection = widget.get_active()
-			if selection != 0:
-				prompt_add(dicts.counters[selection])
-
-		self.countfiles.connect("changed", set_countfiles)
-
-		self.countprocesses = gtkbuilder.get_object("countprocesses")
-
-		def set_countprocesses(widget, data=None):
-			prompt_add("\\$(count_processes)")
-
-		self.countprocesses.connect("clicked", set_countprocesses)
-
-		self.showuptime = gtkbuilder.get_object("showuptime")
-
-		def set_showuptime(widget, data=None):
-			prompt_add("\\$(show_uptime)")
-
-		self.showuptime.connect("clicked", set_showuptime)
-
-		self.showload = gtkbuilder.get_object("showload")
-		self.showload.set_active(0)
-
-		def set_showload(widget, data=None):
-			selection = widget.get_active()
-			if selection != 0:
-				prompt_add(dicts.load_getters[selection])
-
-		self.showload.connect("changed", set_showload)
-
-		self.showtty = gtkbuilder.get_object("showtty")
-
-		def set_showtty(widget, data=None):
-			prompt_add("\\$(showtty)")
-
-		self.showtty.connect("clicked", set_showtty)
-
-		self.showmem = gtkbuilder.get_object("showmem")
-		self.showmem.set_active(0)
-
-		def set_showmem(widget, data=None):
-			selection = widget.get_active()
-			if selection != 0:
-				prompt_add(dicts.memory_getters[selection])
-
-		self.showmem.connect("changed", set_showmem)
-
-		self.showcpuload = gtkbuilder.get_object("showcpuload")
-
-		def set_showcpuload(widget, data=None):
-			prompt_add("\\$(show_cpu_load)")
-
-		self.showcpuload.connect("clicked", set_showcpuload)
-
-		self.showbatteryload = gtkbuilder.get_object("showbatteryload")
-		self.showbatteryload.set_active(0)
-
-		def set_showbatteryload(widget, data=None):
-			selection = widget.get_active()
-			if selection != 0:
-				prompt_add(dicts.battery_getters[selection])
-
-		self.showbatteryload.connect("changed", set_showbatteryload)
-
-		self.showspace = gtkbuilder.get_object("showspace")
-		self.showspace.set_active(0)
-
-		def set_showspace(widget, data=None):
-			selection = widget.get_active()
-			if selection != 0:
-				prompt_add(dicts.space_getters[selection])
-
-		self.showspace.connect("changed", set_showspace)
-
-		self.showseconds = gtkbuilder.get_object("showseconds")
-
-		def set_showseconds(widget, data=None):
-			prompt_add("${SECONDS}")
-
-		self.showseconds.connect("clicked", set_showseconds)
-
+				
+		###
+		
 		self.empty_pc = gtkbuilder.get_object("empty_pc")
-
+		
 		def do_empty_pc(widget, data=None):
 			self.prompt_command_buffer.set_text("")
 
@@ -345,15 +148,55 @@ class PromptBuilder(object):
 
 		self.redo_ps1.connect("clicked", do_redo_ps1)
 
-		self.insert_color = gtkbuilder.get_object("insert_color")
-		self.insert_color.set_active(0)
+		self.show_toolbox = gtkbuilder.get_object("show_toolbox")
 
-		def do_insert_color(widget, data=None):
-			selection = widget.get_active()
-			if selection != 0:
-				prompt_add(dicts.symbolic_colors[selection])
+		def do_show_toolbox(widget, data=None):
+			toolbox = gtkbuilder.get_object("Toolbox")
+			toolbox.show_all()
+			toolbox.connect("delete-event", lambda w, e: w.hide() or True)
 
-		self.insert_color.connect("changed", do_insert_color)
+		self.show_toolbox.connect("clicked", do_show_toolbox)
+
+		######
+		def load_toolbutton(object, text):
+			widget = gtkbuilder.get_object("%s" % object)
+			widget.connect("clicked", prompt_add, text)
+			
+		load_toolbutton("username", "\\u")
+		load_toolbutton("hostname", "\\h")
+		load_toolbutton("fhostname", "\\H")
+		load_toolbutton("time", "\\t")
+		load_toolbutton("date", "\\d")
+		load_toolbutton("sign", "\\$")
+		load_toolbutton("fworkdir", "\\w")
+		load_toolbutton("workdir", "\\W")
+		load_toolbutton("euid", "$EUID")
+		load_toolbutton("jobs", "\\j")
+		load_toolbutton("bang", "\\!")
+		load_toolbutton("number", "\\#")
+		load_toolbutton("pid", "$BASHPID")
+		load_toolbutton("shlvl", "$SHLVL")
+		load_toolbutton("truncpwd", "\\$(trunc_pwd)")
+		load_toolbutton("showsize", "\\$(show_size)")
+		load_toolbutton("countprocesses", "\\$(count_processes)")
+		load_toolbutton("showuptime", "\\$(show_uptime)")
+		load_toolbutton("showtty", "\\$(showtty)")
+		load_toolbutton("showcpuload", "\\$(show_cpu_load)")
+		load_toolbutton("showseconds", "\\$SECONDS)")
+		
+		###
+		
+		def load_toolcombo(object, dict):
+			widget = gtkbuilder.get_object("%s" % object)
+			widget.set_active(0)
+			widget.connect("changed", prompt_add_combo, dict)
+			
+		load_toolcombo("showmem", dicts.memory_getters)
+		load_toolcombo("showbatteryload", dicts.battery_getters)
+		load_toolcombo("showspace", dicts.space_getters)
+		load_toolcombo("countfiles", dicts.counters)
+		load_toolcombo("showload", dicts.load_getters)
+		load_toolcombo("insert_color", dicts.symbolic_colors)
 
 		self.insert_prompt = gtkbuilder.get_object("insert_prompt")
 		self.insert_prompt.set_active(0)
