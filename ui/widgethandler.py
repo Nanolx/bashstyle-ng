03a7ff5a8f0b7c94817be301b99f89a3e5be5021
Christopher Roy Bratusek <nano@tuxfamily.org>
2013-03-23 09:24:04 +0100

    simply config handling in widgethandler.py

--- ui/widgethandler.py
+++ ui/widgethandler.py
@@ -37,7 +37,12 @@ gtkbuilder.add_from_file(PREFIX + "/share/bashstyle-ng/ui/bashstyle8.ui")
 
 class WidgetHandler(object):
 		####################### metafuncs for handling widgets ###########################
-		def InitWidget(self, widget, group, setting, type, dict, cfo, udc, fdc):
+		def __init__(self, cfo, udc, fdc):
+			self.config = cfo
+			self.userdefault = udc
+			self.factorydefault = fdc
+
+		def InitWidget(self, widget, group, setting, type, dict):
 
 			def LoadWidget(widget):
 				object = gtkbuilder.get_object("%s" % widget)
@@ -45,13 +50,13 @@ class WidgetHandler(object):
 
 			def LoadValue(object, group, setting, type):
 				if type == "text":
-					object.set_text("%s" % cfo["%s" % group]["%s" % setting])
+					object.set_text("%s" % self.config["%s" % group]["%s" % setting])
 				elif type == "int":
-					object.set_value(cfo["%s" % group].as_int("%s" % setting))
+					object.set_value(self.config["%s" % group].as_int("%s" % setting))
 				elif type == "bool":
-					object.set_active(cfo["%s" % group].as_bool("%s" % setting))
+					object.set_active(self.config["%s" % group].as_bool("%s" % setting))
 				elif type == "combo":
-					object.set_active(misc.SwapDictionary(dict)[cfo["%s" % group]["%s" % setting]])
+					object.set_active(misc.SwapDictionary(dict)[self.config["%s" % group]["%s" % setting]])
 
 			def ConnectSignals(object, type, widget_group, widget_setting):
 				if type == "text":
@@ -69,26 +74,26 @@ class WidgetHandler(object):
 			def revert_option(widget, pos, event, type, widget_group, widget_setting):
 				if type == "text" or type == "int":
 					if pos == Gtk.EntryIconPosition.SECONDARY:
-						opt = fdc["%s" % widget_group]["%s" % widget_setting]
+						opt = self.factorydefault["%s" % widget_group]["%s" % widget_setting]
 					else:
-						opt = udc["%s" % widget_group]["%s" % widget_setting]
-					cfo["%s" % widget_group]["%s" % widget_setting] = opt
+						opt = self.userdefault["%s" % widget_group]["%s" % widget_setting]
+					self.config["%s" % widget_group]["%s" % widget_setting] = opt
 					if type == "text":
-						widget.set_text("%s" % cfo["%s" % widget_group]["%s" % widget_setting])
+						widget.set_text("%s" % self.config["%s" % widget_group]["%s" % widget_setting])
 					elif type == "int":
-						widget.set_value(cfo["%s" % widget_group].as_int("%s" % widget_setting))
+						widget.set_value(self.config["%s" % widget_group].as_int("%s" % widget_setting))
 
 			def set_option(widget, type, dict, widget_group, widget_setting):
 				if type == "text":
-					cfo["%s" % widget_group]["%s" % widget_setting] = widget.get_text()
+					self.config["%s" % widget_group]["%s" % widget_setting] = widget.get_text()
 				elif type == "int":
-					cfo["%s" % widget_group]["%s" % widget_setting] = widget.get_value_as_int()
+					self.config["%s" % widget_group]["%s" % widget_setting] = widget.get_value_as_int()
 				elif type == "bool":
-					cfo["%s" % widget_group]["%s" % widget_setting] = widget.get_active()
+					self.config["%s" % widget_group]["%s" % widget_setting] = widget.get_active()
 					if widget_setting == "use_bashstyle":
 						misc.EnableBashstyleNG(widget.get_active())
 				elif type == "combo":
-					cfo["%s" % widget_group]["%s" % widget_setting] =  dict[widget.get_active()]
+					self.config["%s" % widget_group]["%s" % widget_setting] =  dict[widget.get_active()]
 
 			def emit_text(widget, text, *args):
 				if text in blacklist:
