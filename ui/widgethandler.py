4835e3a7fcbe8f3f5cc88672d0fe140c0ba593c8
Christopher Roy Bratusek <nano@tuxfamily.org>
2014-03-25 23:28:37 +0100

    keybinding stuff part 2: - add Granite framework as dependency - fully working UI (settings are not yet applied)

--- ui/widgethandler.py
+++ ui/widgethandler.py
@@ -25,13 +25,19 @@ try:
 except ImportError:
 	FAILED.append("Gtk (from gi.repository)")
 
+try:
+	from gi.repository import Granite
+except ImportError:
+	FAILED.append("Granite (from gi.repository)")
+
 if FAILED:
     print("The following modules failed to import: %s" % (" ".join(FAILED)))
     sys.exit(1)
 
 PREFIX = os.getenv('BSNG_UI_PREFIX')
 DATADIR = os.getenv('BSNG_DATADIR')
-blacklist = ['', '\"'] #\'
+blacklist = ['\'', '\"']
+blacklist_key = ['\'' '\"', ':' ]
 gtkbuilder = Gtk.Builder()
 gtkbuilder.set_translation_domain("bs-ng")
 gtkbuilder.add_from_file(DATADIR + "/bashstyle-ng/ui/bashstyle8.ui")
@@ -46,8 +52,23 @@ class WidgetHandler(object):
 		def InitWidget(self, widget, group, setting, type, dict):
 
 			def LoadWidget():
-				object = gtkbuilder.get_object("%s" % widget)
+				if type == "key":
+					object = Granite.WidgetsModeButton()
+				else:
+					object = gtkbuilder.get_object("%s" % widget)
 				return object
+
+			def PrepareKeyWidget():
+				label = gtkbuilder.get_object("%s.label" % setting)
+				object.append_text("Alt")
+				object.append_text("Ctrl")
+				grid = gtkbuilder.get_object("keybindings.table")
+				grid.attach_next_to(object, label, Gtk.PositionType.RIGHT, 1, 1)
+				object.set_visible(True)
+
+			def LoadKeyEntry():
+				object_entry = gtkbuilder.get_object("%s.entry" % setting)
+				return object_entry
 
 			def LoadValue():
 				if type == "text":
@@ -58,19 +79,38 @@ class WidgetHandler(object):
 					object.set_active(self.config["%s" % group].as_bool("%s" % setting))
 				elif type == "combo":
 					object.set_active(misc.SwapDictionary(dict)[self.config["%s" % group]["%s" % setting]])
+				elif type == "key":
+					if self.config["%s" % group]["%s" % setting] != "":
+						modifier = self.config["%s" % group]["%s" % setting].split(":")[0]
+						boundkey = self.config["%s" % group]["%s" % setting].split(":")[1]
+					else:
+						modifier = ""
+						boundkey = ""
+					if modifier == "e":
+						object.set_selected(0)
+					elif modifier == "C":
+						object.set_selected(1)
+					else:
+						object.set_selected(-1)
+					object_entry.set_text(boundkey)
 
 			def ConnectSignals():
 				if type == "text":
 					object.connect("insert-text", emit_text)
 					object.connect("icon-press", revert_option, type, group, setting)
-					object.connect("changed", set_option, type, None, group, setting)
+					object.connect("changed", set_option, None, type, None, group, setting)
 				elif type == "int":
-					object.connect("value-changed", set_option, type, None, group, setting)
+					object.connect("value-changed", set_option, None, type, None, group, setting)
 					object.connect("icon-press", revert_option, type, group, setting)
 				elif type == "bool":
-					object.connect("toggled", set_option, type, None, group, setting)
+					object.connect("toggled", set_option, None, type, None, group, setting)
 				elif type == "combo":
-					object.connect("changed", set_option, type, dict, group, setting)
+					object.connect("changed", set_option, None, type, dict, group, setting)
+				elif type == "key":
+					object.connect("mode-changed", set_option, type, None, group, setting)
+					object_entry.connect("insert-text", emit_keytext)
+					object_entry.connect("changed", set_option, None, type, None, group, setting)
+					object_entry.connect("icon-press", revert_option, type, group, setting)
 
 			def revert_option(widget, pos, event, type, widget_group, widget_setting):
 				if type == "text" or type == "int":
@@ -83,8 +123,24 @@ class WidgetHandler(object):
 						widget.set_text("%s" % self.config["%s" % widget_group]["%s" % widget_setting])
 					elif type == "int":
 						widget.set_value(self.config["%s" % widget_group].as_int("%s" % widget_setting))
+				elif type == "key":
+					if pos == Gtk.EntryIconPosition.SECONDARY:
+						opt = self.factorydefault["%s" % widget_group]["%s" % widget_setting]
+					if opt != "":
+						modifier = opt.split(":")[0]
+						boundkey = opt.split(":")[1]
+					else:
+						modifier = ""
+						boundkey = ""
+					if modifier == "e":
+						object.set_selected(0)
+					elif modifier == "C":
+						object.set_selected(1)
+					else:
+						object.set_selected(-1)
+					object_entry.set_text(boundkey)
 
-			def set_option(widget, type, dict, widget_group, widget_setting):
+			def set_option(widget, data, type, dict, widget_group, widget_setting):
 				if type == "text":
 					self.config["%s" % widget_group]["%s" % widget_setting] = widget.get_text()
 				elif type == "int":
@@ -93,66 +149,30 @@ class WidgetHandler(object):
 					self.config["%s" % widget_group]["%s" % widget_setting] = widget.get_active()
 				elif type == "combo":
 					self.config["%s" % widget_group]["%s" % widget_setting] =  dict[widget.get_active()]
+				elif type == "key":
+					if object_entry.get_text() != "":
+						if object.get_selected() == 0:
+							new_mod = "e"
+						elif object.get_selected() == 1:
+							new_mod = "C"
+						else:
+							new_mod = "C"
+							print("no modifier selected, fallback to C")
+						self.config["%s" % widget_group]["%s" % widget_setting] = new_mod + ":" + object_entry.get_text()
+					else:
+						self.config["%s" % widget_group]["%s" % widget_setting] = ""
 
 			def emit_text(widget, text, *args):
 				if text in blacklist:
+					widget.emit_stop_by_name('insert-text')
+
+			def emit_keytext(widget, text, *args):
+				if text in blacklist_key:
 					widget.emit_stop_by_name('insert-text')
 
 			object = LoadWidget()
+			if type == "key":
+				PrepareKeyWidget()
+				object_entry = LoadKeyEntry()
 			LoadValue()
 			ConnectSignals()
-
-		def InitKeyWidget(self, setting):
-			group = "Keybindings"
-			modifier = self.config["%s" % group]["%s" % setting].split(":")[0]
-			boundkey = self.config["%s" % group]["%s" % setting].split(":")[1]
-
-			def LoadWidgets():
-				object_alt = gtkbuilder.get_object("%s.alt" % setting)
-				object_ctrl = gtkbuilder.get_object("%s.ctrl" % setting)
-				object_entry = gtkbuilder.get_object("%s.entry" % setting)
-				object_button = gtkbuilder.get_object("%s.delete" % setting)
-				return object_alt, object_ctrl, object_entry, object_button
-
-			def LoadValues():
-				if modifier == "e":
-					object_alt.set_active(True)
-				else:
-					object_ctrl.set_active(True)
-				object_entry.set_text(boundkey)
-
-			def ConnectSignals():
-				object_entry.connect("insert-text", emit_text)
-				object_entry.connect("changed", set_option)
-				object_alt.connect("toggled", set_option)
-				object_ctrl.connect("toggled", set_option)
-				object_button.connect("clicked", revert_option)
-
-			def revert_option(widget):
-				old_opt = self.factorydefault["%s" % group]["%s" % setting]
-				old_mod = old_opt.split(":")[0]
-				old_key = old_opt.split(":")[1]
-				if old_mod == "e":
-					object_alt.set_active(True)
-				else:
-					object_ctrl.set_active(True)
-				object_entry.set_text(old_key)
-
-			def set_option(widget):
-				if object_entry.get_text() != "":
-					if object_alt.get_active() == True:
-						new_mod = "e"
-					else:
-						new_mod = "C"
-					self.config["%s" % group]["%s" % setting] = new_mod + ":" + object_entry.get_text()
-				else:
-					self.config["%s" % group]["%s" % setting] = ""
-
-			def emit_text(widget, text, *args):
-				if text in blacklist:
-					widget.emit_stop_by_name('insert-text')
-
-			object_alt, object_ctrl, object_entry, object_button = LoadWidgets()
-			LoadValues()
-			ConnectSignals()
-
