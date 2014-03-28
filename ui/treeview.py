7a19aa295c503ba5c3ea0f24ec28a41abc3b62d0
Christopher Roy Bratusek <nano@tuxfamily.org>
2014-03-28 17:26:18 +0100

    keybindings finished

--- ui/treeview.py
+++ ui/treeview.py
@@ -36,6 +36,29 @@ if FAILED:
 
 gtkbuilder = widgethandler.gtkbuilder
 
+keybindings = {
+	"undo",
+	"upcase_word",
+	"capitalize_word",
+	"downcase_word",
+	"transpose_words",
+	"transpose_chars",
+	"unix_word_rubout",
+	"kill_word",
+	"possible_filename_completions",
+	"possible_hostname_completions",
+	"possible_username_completions",
+	"possible_variable_completions",
+	"kill_line",
+	"unix_line_discard",
+	"beginning_of_line",
+	"end_of_line",
+	"clear_screen",
+	"history_search_forward",
+	"history_search_backward",
+	"complete_path"
+}
+
 class Tree(object):
 
 	def __init__(self, cfo, udc, fdc):
@@ -75,16 +98,49 @@ class Tree(object):
 
 		def text_edited(widget, path, text):
 			store[path][3] = text
-			print(store[path][0], store[path][1], store[path][2], store[path][3])
+			self.change_setting(store[path][0].replace("-", "_"), store[path][1],
+					store[path][2], store[path][3])
 
 		render_key.connect("edited", text_edited)
 
 		def on_changed(selection):
 			(model, iter) = selection.get_selected()
-			print("%s %s %s %s" %(model[iter][0],  model[iter][1], model[iter][2], model[iter][3]))
-			return True
+			self.change_setting(model[iter][0].replace("-", "_"), model[iter][1],
+					model[iter][2], model[iter][3])
 
 		tree.get_selection().connect("changed", on_changed)
 
-		store.append(["unix-discard-line", False, True, "u"])
-		store.append(["kill-line", True, False, "k"])
+		for key in keybindings:
+			modifier, boundkey, label = self.prepare(key)
+			if modifier == "e":
+				alt = True
+				ctrl = False
+			elif modifier == "C":
+				alt = False
+				ctrl = True
+			else:
+				alt = False
+				ctrl = False
+			store.append([label, alt, ctrl, boundkey])
+
+	def prepare(self, setting):
+		value = self.config["Keybindings"][setting]
+		if value == "":
+			modifier = ""
+			boundkey = ""
+		else:
+			modifier = value.split(":")[0]
+			boundkey = value.split(":")[1]
+		label = setting.replace("_", "-")
+		return modifier, boundkey, label
+
+	def change_setting(self, setting, alt, ctrl, key):
+		if key == "":
+			self.config["Keybindings"][setting] = ""
+		else:
+			if alt == True:
+				self.config["Keybindings"][setting] = "e:" + key
+			elif ctrl == True:
+				self.config["Keybindings"][setting] = "C:" + key
+			else:
+				self.config["Keybindings"][setting] = ""
