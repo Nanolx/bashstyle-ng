1a6bd5f8dac1425f481b6c3c9b36717987523b83
Christopher Roy Bratusek <nano@jpberlin.de>
2016-03-23 21:47:11 +0100

    mass commit featuring: - fixed bug #216, #217, #218 - make 'colorshell' no longer pollute PS1...4 variables - Don't mark setting's name and a few others as translatable strings. - ove last command recording to the end of BashStyle-NG initialization - Less environment pollution by unseting a lot of variables which are of no use after @code{BashStyle-NG} initialization. - Simplified color setup mechanism for non/echoable prompt part colors. - minor coding style changes / improvements

--- ui/config.py
+++ ui/config.py
@@ -31,7 +31,7 @@ FACTORY_DEFAULTS = (DATADIR + '/bashstyle-ng/bs-ng.ini')
 VENDOR_DEFAULTS = ('/etc/bs-ng_vendor.ini')
 NXRC = (DATADIR + "/bashstyle-ng/rc/nx-rc")
 
-app_ini_version = 28
+app_ini_version = 29
 
 class Config(object):
 	def InitConfig(self):
