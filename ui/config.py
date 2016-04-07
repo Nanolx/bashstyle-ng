aabfc087f9f53abbd50fb6c9ae08ce7399322afa
Christopher Roy Bratusek <nano@jpberlin.de>
2016-04-07 20:12:33 +0200

    bump known ini version

--- ui/config.py
+++ ui/config.py
@@ -31,7 +31,7 @@ FACTORY_DEFAULTS = (DATADIR + '/bashstyle-ng/bs-ng.ini')
 VENDOR_DEFAULTS = ('/etc/bs-ng_vendor.ini')
 NXRC = (DATADIR + "/bashstyle-ng/rc/bashstyle-rc")
 
-app_ini_version = 30
+app_ini_version = 31
 
 class Config(object):
 	def InitConfig(self):
