577772ddff89dd82c2a03f12efa49b65ccaef892
Christopher Roy Bratusek <nano@jpberlin.de>
2017-02-26 17:39:51 +0100

    Install bashstyle-config-helper launcher script.

--- ui/config-update-helper.py
+++ ui/config-update-helper.py
@@ -27,6 +27,19 @@ class ConfigUpdateHelper(object):
 				\nBashStyle-NG configuration update helper.\
 				\nLicensed under the GNU GENERAL PUBLIC LICENSE v3"))
 
+		parser.add_option("-v", "--version", dest="version",
+				  action="store_true", default=False, help=_("print version and exit"))
+
+		parser.add_option("-p", "--prefix", dest="prefix",
+				  action="store_true", default=False, help=_("print installation prefix and exit"))
+
+		parser.add_option("-P", "--python", dest="python",
+				  action="store_true", default=False, help=_("print used Python interpreter;\
+	 if additional args are given they will be passed to the used Python interpreter."))
+
+		parser.add_option("-d", "--doc", dest="doc",
+				  action="store_true", default=False, help=_("open HTML documentation and exit"))
+
 		parser.add_option("-u", "--update", dest="update",
 				action="store_true", default=False, help=_("update user configuration and exit"))
 
@@ -40,6 +53,14 @@ class ConfigUpdateHelper(object):
 				  action="store_true", default=False, help=_("reset user configuration and exit"))
 
 		(options, args) = parser.parse_args()
+
+		if options.version:
+			print("%s" % os.getenv('BSNG_VERSION'))
+			sys.exit(0)
+
+		if options.prefix:
+			print("%s" % os.getenv('BSNG_PREFIX'))
+			sys.exit(0)
 
 		if options.update:
 			cfg.InitConfig()
