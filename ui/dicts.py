3bd04989a8d0f3c4f528e7d3ee691212fde4121c
Christopher Roy Bratusek <nano@jpberlin.de>
2020-03-22 19:50:20 +0100

    ui: change order

--- ui/dicts.py
+++ ui/dicts.py
@@ -267,9 +267,9 @@ keybindings = {
 iconview_icons = ["bashstyle-ng-style", "bashstyle-ng-alias", "bashstyle-ng-advanced",
                   "bashstyle-ng-shopts", "bashstyle-ng-git", "bashstyle-ng-readline",
                   "bashstyle-ng-vim", "bashstyle-ng-nano", "bashstyle-ng-ls",
-                  "bashstyle-ng-man", "bashstyle-ng-keys", "bashstyle-ng-custom",
-                  "bashstyle-ng-config", "bashstyle-ng-doc", "bashstyle-ng-info",
-                  "bashstyle-ng-gcc", "bashstyle-ng-shell"]
+                  "bashstyle-ng-man", "bashstyle-ng-gcc", "bashstyle-ng-keys",
+                  "bashstyle-ng-custom", "bashstyle-ng-config", "bashstyle-ng-doc",
+                  "bashstyle-ng-info", "bashstyle-ng-shell"]
 
 iconview_labels = {
     "bashstyle-ng-style": _("General Style"),
