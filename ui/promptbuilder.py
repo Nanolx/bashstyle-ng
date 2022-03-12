0b192f906ea8e36ea31cad8956a2c2d48c442c39
Christopher Roy Bratusek <nano@jpberlin.de>
2022-03-12 10:07:49 +0100

    remove copyright year from all files, except docs

--- ui/promptbuilder.py
+++ ui/promptbuilder.py
@@ -5,7 +5,7 @@
 #							#
 # Licensed under GNU GENERAL PUBLIC LICENSE v3		#
 #							#
-# Copyright 2007 - 2020 Christopher Bratusek		#
+# Copyright Christopher Bratusek			#
 #							#
 #########################################################
 
