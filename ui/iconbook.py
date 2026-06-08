f7e19c577a7dd283b1268824ce1eaae3399774af
Christopher Roy Bratušek <nano@jpberlin.de>
2026-06-08 23:31:49 +0200

    gtk4 replace IconView with GridView

--- ui/iconbook.py
+++ ui/iconbook.py
@@ -23,14 +23,9 @@ try:
     gi.require_version("Gtk", "4.0")
     from gi.repository import Gtk
     gi.require_version("Gdk", "4.0")
-    from gi.repository import Gdk, GdkPixbuf
+    from gi.repository import Gdk, Gio, GObject
 except ImportError:
     FAILED.append(_("Gtk (from gi.repository)"))
-
-try:
-    from gi.repository.GdkPixbuf import Pixbuf
-except ImportError:
-    FAILED.append(_("GdkPixbuf (from gi.repository)"))
 
 if FAILED:
     print(_("The following modules failed to import: %s")
@@ -39,58 +34,117 @@ if FAILED:
 
 gtkbuilder = widgethandler.gtkbuilder
 
+class IconItem(GObject.Object):
+    def __init__(self, name, label):
+        super().__init__()
+        self.name = name
+        self.label = label
+
 class IconBook(object):
-
     def InitIconBook(self):
+        self.gridview = gtkbuilder.get_object("iconview")
+        self.model = Gio.ListStore(item_type=IconItem)
 
-        liststore = gtkbuilder.get_object("iconviewstore")
-        iconview = gtkbuilder.get_object("iconview")
-        iconview.set_model(liststore)
-        iconview.set_pixbuf_column(0)
-        iconview.set_text_column(1)
-        iconview.set_activate_on_single_click(True)
+        selection = Gtk.SingleSelection(model=self.model)
+        selection.connect("selection-changed", self.on_selection_changed)
+        self.gridview.set_model(selection)
 
-        notebook = gtkbuilder.get_object("notebook")
-        notebook.set_current_page(0)
+        factory = Gtk.SignalListItemFactory()
+        factory.connect("setup", self.on_setup_grid_item)
+        factory.connect("bind", self.on_bind_grid_item)
+        self.gridview.set_factory(factory)
 
-        main_label = gtkbuilder.get_object("main.label")
-        main_label.set_visible(0)
-
-        def back_clicked(data):
-            notebook.set_current_page(0)
-            back.set_visible(0)
-            main_label.set_visible(0)
-
-        back = gtkbuilder.get_object("back")
-        back.connect("clicked", back_clicked)
-        back.set_visible(0)
-
-        display = Gdk.Display.get_default()
-        icon_theme = Gtk.IconTheme.get_for_display(display)
+        self.notebook = gtkbuilder.get_object("notebook")
+        self.notebook.set_current_page(0)
+        self.main_label = gtkbuilder.get_object("main.label")
+        self.main_label.set_visible(False)
+        self.back = gtkbuilder.get_object("back")
+        self.back.set_visible(False)
+        self.back.connect("clicked", self.back_clicked)
 
         for icon in dicts.iconview_icons:
-            icon_lookup = icon_theme.lookup_icon(icon, None, 32, 1,
-            Gtk.TextDirection.NONE, Gtk.IconLookupFlags.FORCE_SYMBOLIC)
-            icon_file = icon_lookup.get_file()
-            icon_path = icon_file.get_path()
-            pixbuf = GdkPixbuf.Pixbuf.new_from_file(icon_path)
-            liststore.append([pixbuf, dicts.iconview_labels[icon]])
+            label = dicts.iconview_labels[icon]
+            self.model.append(IconItem(icon, label))
 
-        def iconview_activated(widget, item):
-            model = widget.get_model()
-            if model[item][1] == _("Documentation"):
-                back.set_visible(0)
-                subprocess.Popen(
-                    ["xdg-open", "%s" % os.getenv('BSNG_DATADIR') +
-                     "/doc/bashstyle-ng/index.html"]
-                )
-            elif model[item][1] == _("Start Terminal"):
-                back.set_visible(0)
-                subprocess.Popen(["x-terminal-emulator"])
-            else:
-                notebook.set_current_page(dicts.notebook_pages[model[item][1]])
-                back.set_visible(1)
-                main_label.set_visible(1)
-                main_label.set_text(_("Category: ") + _(model[item][1]))
+        provider = Gtk.CssProvider()
+        provider.load_from_data("""
+            gridview child {
+                padding: 10px;
+                border-radius: 8px;
+                transition: all 200ms ease-out;
+            }
+            gridview child:hover {
+                background-color: alpha(@theme_fg_color, 0.1);
+                transform: translateY(-2px);
+            }
+            gridview child:focus {
+                background-color: alpha(@theme_selected_bg_color, 0.2);
+                outline: 2px solid alpha(@theme_selected_bg_color, 0.5);
+            }
+            gridview child:hover image {
+                transform: scale(1.15);
+                transition: transform 200ms ease-out;
+            }
+            gridview child:hover label {
+                font-weight: bold;
+                color: @theme_selected_bg_color;
+            }
+            gridview child image {
+                margin-bottom: 5px;
+                filter: drop-shadow(0 2px 2px rgba(0,0,0,0.1));
+            }
+        """.encode())
 
-        iconview.connect("item-activated", iconview_activated)
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(),
+            provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+
+    def on_setup_grid_item(self, factory, list_item):
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
+        box.set_margin_top(10)
+        box.set_margin_bottom(10)
+
+        icon = Gtk.Image(pixel_size=32)
+        label = Gtk.Label()
+
+        box.append(icon)
+        box.append(label)
+        list_item.set_child(box)
+
+    def on_bind_grid_item(self, factory, list_item):
+        row_item = list_item.get_item()
+        box = list_item.get_child()
+        icon_widget = box.get_first_child()
+        label_widget = box.get_last_child()
+
+        icon_widget.set_from_icon_name(row_item.name)
+        label_widget.set_label(row_item.label)
+
+    def on_selection_changed(self, selection, position, n_items):
+        item = selection.get_selected_item()
+        if not item:
+            return
+
+        label = item.label
+        if label == _("Documentation"):
+            self.back.set_visible(False)
+            path = os.getenv('BSNG_DATADIR') + "/doc/bashstyle-ng/index.html"
+            subprocess.Popen(["xdg-open", path])
+        elif label == _("Start Terminal"):
+            self.back.set_visible(False)
+            subprocess.Popen(["x-terminal-emulator"])
+        else:
+            self.notebook.set_current_page(dicts.notebook_pages[label])
+            self.back.set_visible(True)
+            self.main_label.set_visible(True)
+            self.main_label.set_text(_("Category: ") + _(label))
+
+        selection.set_selected(Gtk.INVALID_LIST_POSITION)
+
+    def back_clicked(self, btn):
+        self.notebook.set_current_page(0)
+        self.back.set_visible(False)
+        self.main_label.set_visible(False)
+        self.gridview.get_model().set_selected(Gtk.INVALID_LIST_POSITION)
