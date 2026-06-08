bc7728675bdf0d360e36406b03760b844b97b782
Christopher Roy Bratušek <nano@jpberlin.de>
2026-06-08 23:50:44 +0200

    gtk4 improve GtkGridView

--- ui/iconbook.py
+++ ui/iconbook.py
@@ -44,10 +44,8 @@ class IconBook(object):
     def InitIconBook(self):
         self.gridview = gtkbuilder.get_object("iconview")
         self.model = Gio.ListStore(item_type=IconItem)
-
-        selection = Gtk.SingleSelection(model=self.model)
-        selection.connect("selection-changed", self.on_selection_changed)
-        self.gridview.set_model(selection)
+        no_selection = Gtk.NoSelection(model=self.model)
+        self.gridview.set_model(no_selection)
 
         factory = Gtk.SignalListItemFactory()
         factory.connect("setup", self.on_setup_grid_item)
@@ -55,43 +53,40 @@ class IconBook(object):
         self.gridview.set_factory(factory)
 
         self.notebook = gtkbuilder.get_object("notebook")
-        self.notebook.set_current_page(0)
-        self.main_label = gtkbuilder.get_object("main.label")
-        self.main_label.set_visible(False)
         self.back = gtkbuilder.get_object("back")
         self.back.set_visible(False)
         self.back.connect("clicked", self.back_clicked)
+        self.main_label = gtkbuilder.get_object("main.label")
 
-        for icon in dicts.iconview_icons:
-            label = dicts.iconview_labels[icon]
-            self.model.append(IconItem(icon, label))
+        for icon_name in dicts.iconview_icons:
+            label_text = dicts.iconview_labels.get(icon_name, icon_name)
+            self.model.append(IconItem(icon_name, label_text))
 
+        self.setup_css()
+
+    def setup_css(self):
         provider = Gtk.CssProvider()
         provider.load_from_data("""
             gridview child {
-                padding: 10px;
-                border-radius: 8px;
+                padding: 0;
+                background: transparent;
+            }
+            .icon-card {
+                padding: 15px;
+                border-radius: 12px;
                 transition: all 200ms ease-out;
             }
-            gridview child:hover {
+            gridview child:hover .icon-card {
                 background-color: alpha(@theme_fg_color, 0.1);
                 transform: translateY(-2px);
             }
-            gridview child:focus {
-                background-color: alpha(@theme_selected_bg_color, 0.2);
-                outline: 2px solid alpha(@theme_selected_bg_color, 0.5);
-            }
-            gridview child:hover image {
+            gridview child:hover .icon-card image {
                 transform: scale(1.15);
                 transition: transform 200ms ease-out;
             }
-            gridview child:hover label {
+            gridview child:hover .icon-card label {
                 font-weight: bold;
                 color: @theme_selected_bg_color;
-            }
-            gridview child image {
-                margin-bottom: 5px;
-                filter: drop-shadow(0 2px 2px rgba(0,0,0,0.1));
             }
         """.encode())
 
@@ -103,31 +98,37 @@ class IconBook(object):
 
     def on_setup_grid_item(self, factory, list_item):
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
-        box.set_margin_top(10)
-        box.set_margin_bottom(10)
+        box.add_css_class("icon-card")
 
-        icon = Gtk.Image(pixel_size=32)
-        label = Gtk.Label()
+        img = Gtk.Image(pixel_size=32)
+        lbl = Gtk.Label()
 
-        box.append(icon)
-        box.append(label)
+        box.append(img)
+        box.append(lbl)
         list_item.set_child(box)
 
+        gesture = Gtk.GestureClick()
+        gesture.connect("released", self.on_item_clicked, list_item)
+        box.add_controller(gesture)
+
     def on_bind_grid_item(self, factory, list_item):
-        row_item = list_item.get_item()
+        item = list_item.get_item()
         box = list_item.get_child()
-        icon_widget = box.get_first_child()
-        label_widget = box.get_last_child()
 
-        icon_widget.set_from_icon_name(row_item.name)
-        label_widget.set_label(row_item.label)
+        if box and item:
+            img = box.get_first_child()
+            lbl = box.get_last_child()
 
-    def on_selection_changed(self, selection, position, n_items):
-        item = selection.get_selected_item()
+            img.set_from_icon_name(item.name)
+            lbl.set_label(item.label)
+
+    def on_item_clicked(self, gesture, n_press, x, y, list_item):
+        item = list_item.get_item()
         if not item:
             return
 
         label = item.label
+
         if label == _("Documentation"):
             self.back.set_visible(False)
             path = os.getenv('BSNG_DATADIR') + "/doc/bashstyle-ng/index.html"
@@ -136,15 +137,13 @@ class IconBook(object):
             self.back.set_visible(False)
             subprocess.Popen(["x-terminal-emulator"])
         else:
-            self.notebook.set_current_page(dicts.notebook_pages[label])
+            page_num = dicts.notebook_pages.get(label, 0)
+            self.notebook.set_current_page(page_num)
             self.back.set_visible(True)
             self.main_label.set_visible(True)
             self.main_label.set_text(_("Category: ") + _(label))
-
-        selection.set_selected(Gtk.INVALID_LIST_POSITION)
 
     def back_clicked(self, btn):
         self.notebook.set_current_page(0)
         self.back.set_visible(False)
         self.main_label.set_visible(False)
-        self.gridview.get_model().set_selected(Gtk.INVALID_LIST_POSITION)
